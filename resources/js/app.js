4ade5d25a584c95b6e3961cf76737a50549c90c4
Miles <unknownnn@duck.com>
2024-09-22 22:38:09 +0530

    updated the model

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,13 +1,14 @@
-// getting user name
-
 const btn = document.getElementById('name-submit');
 let name_value = '';
 
-const user_name = localStorage.getItem('username').toUpperCase();
-document.getElementById('username').textContent = `${user_name ? user_name : 'explorer'}`;
+// Check if there's a username in local storage
+const user_name = localStorage.getItem('username');
+document.getElementById('username').textContent = `${user_name ? user_name.toUpperCase() : 'EXPLORER'}`;
 
-btn.addEventListener('click', function () {
+// Add click event listener
+btn.addEventListener('click', function (event) {
+    event.preventDefault(); // Prevent the form from submitting
     name_value = document.getElementById('name-input').value;
     localStorage.setItem('username', name_value);
-    document.getElementById('username').textContent = name_value; 
+    document.getElementById('username').textContent = name_value.toUpperCase(); 
 });
