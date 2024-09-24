8cc7b1fb77466f722337cf320937b033e37f79aa
Miles <unknownnn@duck.com>
2024-09-24 22:00:53 +0530

    updated designs

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,14 @@
-const btn = document.getElementById('name-submit');
-let name_value = '';
+// document.addEventListener('DOMContentLoaded', function () {
+//     const btn = document.getElementById('name-submit');
+//     let name_value = '';
 
-const user_name = localStorage.getItem('username');
-document.getElementById('username').textContent = `${user_name ? user_name.toUpperCase() : 'EXPLORER'}`;
+//     const user_name = localStorage.getItem('username');
+//     document.getElementById('username').textContent = `${user_name ? user_name.toUpperCase() : 'EXPLORER'}`;
 
-btn.addEventListener('click', function (event) {
-    event.preventDefault(); 
-    name_value = document.getElementById('name-input').value;
-    localStorage.setItem('username', name_value);
-    document.getElementById('username').textContent = name_value.toUpperCase(); 
-});
+//     btn.addEventListener('click', function (event) {
+//         event.preventDefault();
+//         name_value = document.getElementById('name-input').value;
+//         localStorage.setItem('username', name_value);
+//         document.getElementById('username').textContent = name_value.toUpperCase(); 
+//     });
+// });
